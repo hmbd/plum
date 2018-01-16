2f54ae758a0d367341609cc0120587138a6b02f4
hmbd <1131909224@qq.com>
2018-01-17 00:46:49 +0800

    浮点数、复数、unicode

---
+++ practise/unicode_help.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	s := "Hello, 世界"
+	fmt.Println(len(s))
+	fmt.Println(utf8.RuneCountInString(s))
+
+	for i := 0; i < len(s); {
+		r, size := utf8.DecodeRuneInString(s[i:])
+		fmt.Printf("%d\t%c\n", i, r)
+		i += size
+	}
+
+	for i, r := range s {
+		fmt.Printf("索引：%d\t%q\t%d\n", i, r, r)
+	}
+
+	s = "プログラム"
+	fmt.Printf("% x\n", s) // "e3 83 97 e3 83 ad e3 82 b0 e3 83 a9 e3 83 a0"
+	r := []rune(s)
+	fmt.Printf("%x\n", r) // "[30d7 30ed 30b0 30e9 30e0]"
+	fmt.Println(string(r)) // "プログラム"
+}
