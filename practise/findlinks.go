59ccbbcf8ee321b9acf06a82869569ab13c432bb
hmbd <1131909224@qq.com>
2018-01-28 23:47:03 +0800

    递归

---
+++ practise/findlinks.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinkes: %v\n", err)
+		os.Exit(1)
+	}
+	visit(doc)
+	outline(nil, doc)
+}
+
+func visit(doc *html.Node) {
+	for _, link := range visit1(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+func visit1(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit1(links, c)
+	}
+	return links
+}
+
+func outline(stack []string, n *html.Node) {
+	if n.Type == html.ElementNode {
+		stack = append(stack, n.Data)
+		fmt.Println(stack)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		outline(stack, c)
+	}
+}
