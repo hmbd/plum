a6130c8c1aa55a54663ef73ff797b21dfb2f5010
seekplum <1131909224m@sina.cn>
2018-11-13 12:04:42 +0800

    并发客户端、服务端通信

---
+++ gopcp/chapter8/reverb2.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"net"
+	"bufio"
+	"time"
+	"fmt"
+	"strings"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+	time.Sleep(delay)
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		go echo(c, input.Text(), 1*time.Second)
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	c.Close()
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		handleConn(conn) // handle one connection at a time
+	}
+}
