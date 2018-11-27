07e82cb0432eb07c0962c8f13cd7de27658e9309
jiandong.huang <1131909224m@sina.cn>
2018-11-27 21:31:03 +0800

    并发的目录遍历

--- tasks.py
+++ tasks.py
@@ -5,4 +5,4 @@ from invoke import task
 
 @task
 def clean(ctx):
-    ctx.run("find . -path ./.git -prune -o -perm 755 -type f | grep -v "./.git" -exec rm -rf {} +", echo=True)
+    ctx.run("find . -path ./.git -prune -o -type f -perm 755 -print -exec rm -rf {} +", echo=True)
